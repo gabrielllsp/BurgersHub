1ed91163307a81f7cb45ff7b42a851ac18dd413b
gabri <gabriel.amoreira@outlook.com>
2024-07-09 20:41:47 -0300

    Creating UseCase and ViewModel Burger.

---
+++ app/src/main/java/com/example/burgershub/domain/usecase/GetBurgersByNameUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.burgershub.domain.usecase
+
+import com.example.burgershub.data.model.BurgerResponse
+import com.example.burgershub.domain.repository.BurgerRepository
+import javax.inject.Inject
+
+class GetBurgersByNameUseCase @Inject constructor(
+    private val burgerRepository: BurgerRepository
+) {
+    suspend operator fun invoke(name: String): List<BurgerResponse> {
+        return burgerRepository.getBurgerByName(name)
+    }
+}
