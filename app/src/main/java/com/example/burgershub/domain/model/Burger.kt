01e8d5972c65b703c3d7dc1dc7bcf61eda91a2fb
gabri <gabriel.amoreira@outlook.com>
2024-07-09 13:52:21 -0300

    Mapping Response Objects to Domain

--- app/src/main/java/com/example/burgershub/domain/model/Burger.kt
+++ app/src/main/java/com/example/burgershub/domain/model/Burger.kt
@@ -3,8 +3,8 @@ package com.example.burgershub.domain.model
 data class Burger(
     val desc: String?,
     val id: Int?,
-    val imageResponses: List<Image?>?,
-    val ingredientResponses: List<Ingredient?>?,
+    val image: List<Image?>?,
+    val ingredient: List<Ingredient?>?,
     val name: String?,
     val price: Double?,
     val veg: Boolean?
